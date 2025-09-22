c1a62398aa5a216248ad76e98950c2574609011d
rakeshramesh <rakesh0612@gmail.com>
2025-09-22 23:26:44 +0300

    Add deployment test file to verify Git sync

---
+++ public_html/test-deployment.php
@@ -0,0 +1,8 @@
+<!-- DEPLOYMENT TEST - Version 2.0 - <?php echo date('Y-m-d H:i:s'); ?> -->
+<h1>Deployment Test</h1>
+<p>If you see this page, your Git deployment is working.</p>
+<p>Current time: <?php echo date('Y-m-d H:i:s'); ?></p>
+<script>
+console.log('Deployment test page loaded successfully');
+console.log('Current timestamp:', new Date().toISOString());
+</script>
